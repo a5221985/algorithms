a10619834f976bf50da67d771933bf7896dc6ca5
technicalrescuers <technical.rescuers@gmail.com>
2019-08-01 08:25:41 +0530

    New Stuff

--- BuildString.java
+++ BuildString.java
@@ -1,8 +1,20 @@
+import java.util.Map;
+import java.util.HashMap;
+
 public class BuildString {
 
+	private Map<String, Integer> memo = new HashMap<>();
+
 	public int cost(int a, int b, String s) {
-		if (s.length() == 1)
-			return a < b? a: b;
+		if (memo.keySet().contains(s))
+			return memo.get(s);
+
+		if (s.length() == 1) {
+			int c = a < b? a: b;
+			memo.put(s, c);
+			return c;
+		}
+
 
 		int min = cost(a, b, s.substring(0, s.length() - 1)) + ((a < b)? a: b);
 		for (int i = 2; i < s.length(); i++) {
@@ -17,11 +29,12 @@ public class BuildString {
 				break;
 		}
 
+		memo.put(s, min);
 		return min;
 	}
 
 	public static void main(String[] args) {
-		String input = "aaaaaaaa";
+		String input = "aaaaaa";
 
 		BuildString bs = new BuildString();
 		System.out.println(bs.cost(4, 5, input));
