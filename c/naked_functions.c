e8a3bee2beb017c08c5c8d99081a19b87db220dc
Brahmaji Mutthoju <abdm03142014@gmail.com>
2021-04-20 09:46:35 +0530

    Refactored

--- c/naked_functions.c
+++ c/naked_functions.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <stdint.h>
 
-int32_t add_c(int32_t sum);
+int32_t _add_c(int32_t sum);
 
 // convention: params are stored in {RDI, RSI, RDX, R10, R8, R9} 
 __attribute__((naked)) int32_t add(int32_t a, int32_t b) {
     __asm volatile ("addl %esi, %edi");
-    __asm volatile ("jmp *%0"::"r"(add_c));
+    __asm volatile ("jmp *%0"::"r"(_add_c));
 }
 
-int32_t add_c(int32_t sum) {
+int32_t _add_c(int32_t sum) {
     return sum;
 }
 
